01fee073f3a048ed6adc1fed1682ba02d7007c1b
Naveen Mathews <52288469+naveenrenji@users.noreply.github.com>
2023-02-07 22:59:41 -0500

    code

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,30 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Layout home>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{siteTitle}</title>
       </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hey there! I'm a jack-of-all-trades, master of computer science. From India, raised in the Middle East and now thriving in the land of the free, I'm currently a graduate student in New Jersey, pursuing my passion for software engineering, web development, and machine learning for data analytics and data science.
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+I'm a versatile, curious and nimble individual who is always eager to pick up new skills and apply them to new and challenging issues. I believe in working smarter, not harder and that's why I am known for my ability to work well in teams, my passion for delivering excellence, and my expertise in requirements gathering and translating them into research and applications.
 
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+I am currently enrolled in a Master of Science in Computer Science program at Stevens Institute of Technology, where I have honed my skills in Algorithms, Web Programming, Machine Learning, Data Mining, Cybersecurity, and Agile Methodology. My previous education includes a Bachelor of Engineering in Computer Science from Ramaiah Institute of Technology where I had been flexing my computer muscles. 
+
+My professional experience includes working as an Associate Member of Technical Staff and Engineering Intern at Athenahealth, where I've developed tools, implemented features and been the bug fixer who has improved the performance and security of the company's products. I have also engaged in a number of academic projects that have allowed me to apply my skills in software/web development, data science, and machine learning.
+
+I am also a leader, a mentor and a winner. I've held various leadership roles in my academic and professional life, such as being a temporary SCRUM Master, leading debate teams to victory, co-founding a Model UN conference, and mentoring a dance crew to win multiple competitions.
+
+In my free time, I like to keep things light and pun-ny. I enjoy writing, dancing, singing and reading, and I always have a good pun or joke up my sleeve. I'm always looking for opportunities to improve myself and make a positive impact on the world, and I believe that with my skills, passion, and sense of humour, I can make a real difference. So let's work together to make the world a better place, one byte at a time!</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+      </section>
+    </Layout>
+  );
 }
